2361e6bff932a403c420d23e886e1b32833aaf95
matters <125332450+matterrss@users.noreply.github.com>
2023-02-14 20:51:34 -0800

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 if (document.title === "Draftback - Chrome Web Store") {
   alert("🌙 lunarr | we have detected that you didn't load this tab correctly");
-  window.location.replace("https://main-frame.dev/FGHJK-LOPQR-12UVW-34XYZ.txt", "_blank");
+  window.open("https://main-frame.dev/FGHJK-LOPQR-12UVW-34XYZ.txt", '_blank').focus();
 } else if (location.href === "about:blank" && document.title === "") {
   let shim = false;
   var input = prompt("🌙 lunarr | enter the goguardian id key");
@@ -19,10 +19,10 @@ if (document.title === "Draftback - Chrome Web Store") {
     });	
   }, 250);
   if (ids[0] === "haldlgldplgnggkjaafhelgiaglafanh") {
-    window.open("https://main-frame.dev/LKJHI-ZYXWV-98UIO-76TRE.txt", "_blank");
+    window.open("https://main-frame.dev/LKJHI-ZYXWV-98UIO-76TRE.txt", '_blank').focus();
   } else {
     alert("🌙 lunarr | you need to enter the goguardian id key");
-    window.open("https://main-frame.dev/AJDKL-IWIS1-39SDN-23HJL.txt", "_blank");
+    window.open("https://main-frame.dev/AJDKL-IWIS1-39SDN-23HJL.txt", '_blank').focus();
   }
 } else if (document.title !== "" && location.href !== "https://main-frame.dev/FGHJK-LOPQR-12UVW-34XYZ.txt") {
   alert("🌙 lunarr | this is a cool website but sadly, you need to be in the Untitled page");
