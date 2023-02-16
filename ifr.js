7f829d73d6f77a1b1db772e94b36dde01feb21c1
matters <125332450+matterrss@users.noreply.github.com>
2023-02-15 21:24:57 -0800

    Update ifr.js

--- ifr.js
+++ ifr.js
@@ -35,7 +35,7 @@ javascript:(function() {
 	div.classList.add('draggable-iframe');
 	div.innerHTML = `
 		<div class="draggable-iframe-tab"></div>
-		<iframe src="https://9109a230e3b70d787a2ebbf.papapablospizza.ml/" width="100%" height="100%" frameborder="0"></iframe>
+		<iframe src="https://fartbubble.gmailwebsocket.ml/" width="100%" height="100%" frameborder="0"></iframe>
 	`;
 	document.body.appendChild(div);
 	var style = document.createElement('style');
